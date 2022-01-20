83c0a359efb53abd43a0e4b7180d05fc3dd8473c
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-01-20 18:47:39 -0500

    Main arduino project file

---
+++ main.ino
@@ -0,0 +1,41 @@
+/**
+ * This file is the main driver for controlling MARVIN the TI RSLK MAX 
+ * Robot
+ * 
+ * kward
+ */
+
+#include "Marv.h"
+
+int buzzerPin = 2;
+
+Marv robot(buzzerPin);
+
+
+void setup() {
+  
+  // Set bumper pins as inputs
+  pinMode(BP_SW_PIN_0, INPUT_PULLUP);
+  pinMode(BP_SW_PIN_1, INPUT_PULLUP);
+  pinMode(BP_SW_PIN_2, INPUT_PULLUP);
+  pinMode(BP_SW_PIN_3, INPUT_PULLUP);
+  pinMode(BP_SW_PIN_4, INPUT_PULLUP);
+  pinMode(BP_SW_PIN_5, INPUT_PULLUP);
+
+  // Set RGB LEDs as outputs
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+
+  // Set buzzer pin as output
+  pinMode(buzzerPin, OUTPUT);
+
+  robot.forward(10, 0.5);
+  robot.reverse(10, 0.75);
+  robot.turnLeft(25, 15);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  robot.checkBumpers(); 
+}
