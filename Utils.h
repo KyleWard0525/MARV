3a19e872d8c64d898f596f58eb36553450735307
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-01-21 13:45:23 -0500

    Added readByte() and readBit()

--- Utils.h
+++ Utils.h
@@ -1,3 +1,4 @@
+
 #ifndef UTILS_H
 #define UTILS_H
 
@@ -6,6 +7,8 @@
  * 
  * kward
  */
+#include <Wire.h>
+#include <Math.h>
 
 // Play a beep sound to the buzzer at a given frequency
 void beep(int freq, int buzzer)
@@ -16,11 +19,46 @@ void beep(int freq, int buzzer)
     delayMicroseconds(freq);
 }
 
+//  Read byte from a register
+uint8_t readByte(uint16_t memAddr, uint8_t reg)
+{
+  uint8_t regByte = 0;  //  Value of byte read from register
 
+  // Open I2C communication with device
+  Wire.beginTransmission(memAddr);
 
+  // Select register to read from
+  Wire.write(reg);
+  
+  // End current communication (to allow for another instruction to be sent)
+  Wire.endTransmission();
 
+  // Open another comm channel with device and read only the selected register
+  Wire.beginTransmission(memAddr);
+  
+  Wire.requestFrom(memAddr, 1);  //  Tell device to only read the first byte
 
+  // Read byte and end transmission
+  regByte = Wire.read();
+  Wire.endTransmission();
 
+  return regByte;
+}
+
+//  Read bit value from device register at given position
+uint8_t readBit(uint16_t memAddr, uint8_t regAddr, int8_t bitPos)
+{
+  uint8_t regByte = readByte(memAddr, regAddr);
+  uint8_t bitVal = (regByte & (1 << bitPos));
+
+  if(bitVal)
+  {
+    return pow(2,bitPos);
+  }
+  else {
+    return 0;
+  }
+}
 
 
 
