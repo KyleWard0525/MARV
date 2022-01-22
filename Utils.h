293b87a629b2e11b686a2f59c7a9756aa120b8e4
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-01-22 16:16:49 -0500

    Removed readByte() and readBit()

--- Utils.h
+++ Utils.h
@@ -7,9 +7,6 @@
  * 
  * kward
  */
-#include <Wire.h>
-#include <Math.h>
-
 // Play a beep sound to the buzzer at a given frequency
 void beep(int freq, int buzzer)
 {
@@ -19,52 +16,7 @@ void beep(int freq, int buzzer)
     delayMicroseconds(freq);
 }
 
-//  Read byte from a register
-uint8_t readByte(uint16_t memAddr, uint8_t reg)
-{
-  uint8_t regByte = 0;  //  Value of byte read from register
 
-  // Open I2C communication with device
-  Wire.beginTransmission(memAddr);
-
-  // Select register to read from
-  Wire.write(reg);
-  
-  // End current communication (to allow for another instruction to be sent)
-  Wire.endTransmission();
-
-  // Open another comm channel with device and read only the selected register
-  Wire.beginTransmission(memAddr);
-  
-  Wire.requestFrom(memAddr, 1);  //  Tell device to only read the first byte
-
-  // Read byte and end transmission
-  regByte = Wire.read();
-  Wire.endTransmission();
-
-  return regByte;
-}
-
-//  Read bit value from device register at given position
-uint8_t readBit(uint16_t memAddr, uint8_t regAddr, int8_t bitPos)
-{
-  // Read whole byte from the register
-  uint8_t regByte = readByte(memAddr, regAddr);
-
-  /*
-   * Select the bit by ANDing(clearing) the whole byte
-   * except for the one bit at bitPos
-   */
-  uint8_t bitVal = (regByte & (1 << bitPos));
-
-  if(bitVal)
-  {
-    return pow(2,bitPos);
-  }
-  else {
-    return 0;
-  }
-}
 
 
 
