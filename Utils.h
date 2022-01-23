f19a2e1c3acb6f5dc10291b6c4679afccff2037c
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-01-23 17:51:18 -0500

    Added lowPassFilter()

--- Utils.h
+++ Utils.h
@@ -1,4 +1,3 @@
-
 #ifndef UTILS_H
 #define UTILS_H
 
@@ -7,6 +6,8 @@
  * 
  * kward
  */
+#include <Math.h>
+
 // Play a beep sound to the buzzer at a given frequency
 void beep(int freq, int buzzer)
 {
@@ -16,7 +17,24 @@ void beep(int freq, int buzzer)
     delayMicroseconds(freq);
 }
 
+// Apply a low-pass filter to the given x,y, and z values
+void lowPassFilter(double* forces, double alpha)
+{
+  // Create filtered variables
+  double Fx = 0;
+  double Fy = 0;
+  double Fz = 0;
 
+  // Apply low-pass filter
+  Fx = forces[0] * alpha + ((1.0 - alpha) * Fx);
+  Fy = forces[1] * alpha + ((1.0 - alpha) * Fy);
+  Fz = forces[2] * alpha + ((1.0 - alpha) * Fz);
+
+  // Return filtered values to array
+  forces[0] = Fx;
+  forces[1] = Fy;
+  forces[2] = Fz;
+}
 
 
 
