8a0c96a58f42e396572434d41d44939f929c2f71
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-01-27 22:29:15 -0500

    API for HC-SR04 Ultrasonic Sensor

---
+++ Ultrasonic.h
@@ -0,0 +1,68 @@
+#ifndef ULTRASONIC_H
+#define ULTRASONIC_H
+/**
+ * This file is meant to be an API for measuring distances using the 
+ * HC-SR04 Ultrasonic Sensor
+ * kward
+ */
+
+class UltrasonicSensor {
+  private:
+    uint16_t echoPin;
+    uint16_t trigPin;
+    
+ public:
+
+  //  Main constructor
+  UltrasonicSensor(uint16_t ePin, uint16_t tPin)
+  {
+    // Set object variables
+    echoPin = ePin;
+    trigPin = tPin;
+  }
+
+  //  Poll distance to nearest object from the sensor (cm)
+  double measure()
+  {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    /*
+     * Set the trigger pin to high for 10 microseconds as the trigger pin
+     * is only activated when a 10 microsecond pulse is applied
+     */
+     digitalWrite(trigPin, HIGH);
+     delayMicroseconds(10);
+     digitalWrite(trigPin, LOW);
+
+     // Read the amoount of time taken in microseconds for echo pin to recieve a signal
+     unsigned long duration = pulseIn(echoPin, HIGH, 38000);
+     
+ 
+     /*
+      * In order to convert the time taken into cm, we must divide the result
+      * by 2 then again by 29.1. 
+      * 
+      * We divide by 2 as the duration contains the amount of time taken
+      * for the signal to travel from the target and back but we only want to measure the distance
+      * to the target.
+      * 
+      * Next, we divide by 29.1 because the speed of sound is 340m/s which is approximately
+      * 29.1 microseconds per centimeter.
+      * 
+      */
+      return (duration / 2) / 29.1;
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+#endif    //  End ULTRASONIC_H
