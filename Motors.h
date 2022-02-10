173de6a3e739636b2c4aaf7b31de4506e54d3922
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-02-10 18:49:43 -0500

    Fixed cmPerPulse calculation by updating wheelDiameter to the correct size

--- Motors.h
+++ Motors.h
@@ -17,10 +17,10 @@ class Motors {
   private:
     const uint16_t pulsesPerMotorRev = 3;             //  Pulses per revolution of the motor shaft
     const uint16_t pulsesPerWheelRev = 1440;          //  Pulses per revolution of the wheels
-    const uint8_t wheelDiameter = 7;                  //  Wheel diameter in cm
+    const uint8_t wheelDiameter = 6;                  //  Wheel diameter in cm
     const uint16_t gearRatio = 120;                   //  120:1 because ratio = (360 / pulses per motor revolution(3)) 
+    uint16_t defaultSpeed = 50;                       //  Default speed (for now)
     double cmPerPulse;                                //  Centimeters traveled per pulse
-
     
 
   public:
@@ -43,8 +43,8 @@ class Motors {
     }
 
     
-    void forward(double dist_cm, uint16_t speed)
-    {
+    void forward(double dist_cm)
+    {      
       // Compute number of pulses needed
       int nPulses = floor(dist_cm / cmPerPulse);
 
@@ -60,8 +60,8 @@ class Motors {
       rightMotor.enableMotor();
 
       // Set motor speed (start moving)
-      leftMotor.setSpeed(speed);
-      rightMotor.setSpeed(speed);
+      leftMotor.setSpeed(defaultSpeed);
+      rightMotor.setSpeed(defaultSpeed);
 
 
      // Wait for motor encoder to read nPulses (distance traveled = dist_cm)
@@ -77,6 +77,85 @@ class Motors {
       
       Serial.println("Pulses needed to travel " + String(dist_cm) + "cm = " + String(nPulses));
       Serial.println("Actual pulses measured: L = " + String(getEncoderLeftCnt()) + " R = " + String(getEncoderRightCnt()));
+    }
+
+    void reverse(double dist_cm)
+    { 
+      // Compute number of pulses needed
+      int nPulses = floor(dist_cm / cmPerPulse);
+
+      // Reset motor encoder counts
+      resetLeftEncoderCnt();
+      resetRightEncoderCnt();
+
+      // Set motor direction and enable motors
+      leftMotor.directionBackward();
+      rightMotor.directionBackward();
+      
+      leftMotor.enableMotor();
+      rightMotor.enableMotor();
+
+      // Set motor speed (start moving)
+      leftMotor.setSpeed(defaultSpeed);
+      rightMotor.setSpeed(defaultSpeed);
+
+
+     // Wait for motor encoder to read nPulses (distance traveled = dist_cm)
+     while(getEncoderLeftCnt() < nPulses && getEncoderRightCnt() < nPulses)
+     {
+        // Do nothing
+        ;
+     }
+     
+      // Stop motors
+      leftMotor.disableMotor();
+      rightMotor.disableMotor();
+      
+      Serial.println("Pulses needed to travel " + String(dist_cm) + "cm = " + String(nPulses));
+      Serial.println("Actual pulses measured: L = " + String(getEncoderLeftCnt()) + " R = " + String(getEncoderRightCnt()));
+    }
+
+    // Turn a given number of degrees (negative=left turn, positive=right turn)
+    void turn(int16_t nDeg)
+    {
+      // Error check
+      if(nDeg < -359)
+      {
+        nDeg = -359;
+      }
+      if(nDeg > 359)
+      {
+        nDeg = 359;
+      }
+
+      /* 
+      *  Compute number of pulses needed
+      *  
+      *  nPulses = (pulses per rev * abs(nDeg)) / 360
+      */
+      int16_t nPulses = (pulsesPerWheelRev * abs(nDeg)) / 360.0;
+
+      // Reset motor encoder counts
+      resetLeftEncoderCnt();
+      resetRightEncoderCnt();
+
+      Serial.println("\nNumber of pulses to turn " + String(nDeg) + " degrees = " + String(nPulses));
+      
+      // Check direction 
+      if(nDeg < 0)
+      {
+        // Set motor directions to turn left
+        leftMotor.directionBackward();
+        rightMotor.directionForward();
+
+        // Enable motors
+        leftMotor.enableMotor();
+        rightMotor.enableMotor();
+
+        // Set motor speed (start moving)
+        leftMotor.setSpeed(defaultSpeed);
+        rightMotor.setSpeed(defaultSpeed);
+      }
     }
 };
 
