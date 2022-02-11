639f041a50e5cddd605e35c1156005ad7ed3e13d
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-02-10 19:18:51 -0500

    Removed motor control functions

--- Marv.h
+++ Marv.h
@@ -45,7 +45,7 @@ class Marv {
 
   
     I2C serialBus;                        //  Reading and writing data of I2C channels
-    Motors motors;                        //  API for precise motor controls
+    Motors motors;                       //  API for precise motor controls
     IMU* imu;                             //  Measuring acceleration and gyro forces
     Morse* morse;                         //  Communcation with the outside world through Morse code
     UltrasonicSensor* sonicSensor;        //  For measuring distance using ultrasound
@@ -66,7 +66,9 @@ class Marv {
       LiquidCrystal_I2C lcdI2C_module(0x27,16,2);
       this->lcd = new LCD(&lcdI2C_module);
 
-      Serial.println("LCD initialized.");
+      // Check if LCD was initialize successfully by trying to read from i2c addr
+      //Serial.println("LCD initialized.");
+      
 
       // Initialize morse object
       this->morse = new Morse(buzzerPin, morseLedPin);
@@ -97,13 +99,9 @@ class Marv {
           // Check if anyone has helped marv in the alloted time
           if(millis() - start >= 250)
           {
-            reverse(20, 1);
+            motors.reverse(5); // Reverse ~5cm
           }
         }
-        
-
-        // If no one saves the robot, try to save itself
-        //reverse(25, 1);
       } else {
         bumpSensors.setStatusLed(1);
       }
@@ -114,121 +112,6 @@ class Marv {
     {
       // Check distance to nearest object
       
-    }
-
-
-    // Move forward at a given speed for a given time (in seconds)
-    void forward(int speed, float duration)
-    {
-      // Compute movement duration in ms
-      int duration_ms = int(duration*1000); 
-      
-      // Set motor direction and enable motors
-      motors.leftMotor.directionForward();
-      motors.rightMotor.directionForward();
-      
-      motors.leftMotor.enableMotor();
-      motors.rightMotor.enableMotor();
-
-      // Set motor speed (start moving)
-      motors.leftMotor.setSpeed(speed);
-      motors.rightMotor.setSpeed(speed);
-
-      // Loop through duration_ms where i is 1ms
-      for(int i = 0; i < duration_ms; i++)
-      {
-        // Check if robot bumps into something
-        checkBumpers();
-
-        // Sleep for 5ms
-        delay(1);
-      }
-      
-      motors.leftMotor.disableMotor();
-      motors.rightMotor.disableMotor();
-    }
-
-    // Move forward at a given speed for a given time (in seconds)
-    void reverse(int speed, float duration)
-    {
-      // Compute movement duration in ms
-      int duration_ms = int(duration*1000); 
-      
-      // Set motor direction and enable motors
-      motors.leftMotor.directionBackward();
-      motors.rightMotor.directionBackward();
-      
-      motors.leftMotor.enableMotor();
-      motors.rightMotor.enableMotor();
-
-      // Set motor speed (start moving)
-      motors.leftMotor.setSpeed(speed);
-      motors.rightMotor.setSpeed(speed);
-
-      // Sleep for duration_ms then turn of motors
-      delay(duration_ms);
-      motors.leftMotor.disableMotor();
-      motors.rightMotor.disableMotor();
-    }
-
-    // Turn left at a given speed
-    void turnLeft(int speed, float duration)
-    {
-      // Compute movement duration in ms
-      int duration_ms = int(duration*1000); 
-      
-      // Set motor directions
-      motors.leftMotor.directionBackward();
-      motors.rightMotor.directionForward();
-      motors.leftMotor.enableMotor();
-      motors.rightMotor.enableMotor();
-
-      // Set motor speed (start moving)
-      motors.leftMotor.setSpeed(speed);
-      motors.rightMotor.setSpeed(speed);
-
-      // Loop through duration_ms where i is 1ms
-      for(int i = 0; i < duration_ms; i++)
-      {
-        // Check if robot bumps into something
-        checkBumpers();
-
-        // Sleep for 5ms
-        delay(1);
-      }
-      
-      motors.leftMotor.disableMotor();
-      motors.rightMotor.disableMotor();
-    }
-
-    // Turn right at a given speed
-    void turnRight(int speed, float duration)
-    {
-      // Compute movement duration in ms
-      int duration_ms = int(duration*1000); 
-      
-      // Set motor directions
-      motors.leftMotor.directionForward();
-      motors.rightMotor.directionBackward();
-      motors.leftMotor.enableMotor();
-      motors.rightMotor.enableMotor();
-
-      // Set motor speed (start moving)
-      motors.leftMotor.setSpeed(speed);
-      motors.rightMotor.setSpeed(speed);
-
-      // Loop through duration_ms where i is 1ms
-      for(int i = 0; i < duration_ms; i++)
-      {
-        // Check if robot bumps into something
-        checkBumpers();
-
-        // Sleep for 5ms
-        delay(1);
-      }
-      
-      motors.leftMotor.disableMotor();
-      motors.rightMotor.disableMotor();
     }
 };
 
