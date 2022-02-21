d3402716f82fac774908d3e147bf24c4e00837bc
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-02-21 10:54:15 -0500

    Added overloaded beep()

--- Utils.h
+++ Utils.h
@@ -8,6 +8,9 @@
  */
 #include <Math.h>
 
+// Create a custom type definition for MARV's internal functions
+typedef void (*marv_func_t)();
+
 // Play a beep sound to the buzzer at a given frequency
 void beep(int freq, int buzzer)
 {
@@ -16,6 +19,34 @@ void beep(int freq, int buzzer)
     digitalWrite(buzzer, 0);
     delayMicroseconds(freq);
 }
+
+// Beep override with duration in ms
+void beep(int freq, int buzzer, double dur)
+{
+  for(int i = 0; i < dur; i++)
+  {
+    beep(freq, buzzer);
+    delay(1);
+  }
+  
+}
+
+void alarm(int freq, int buzzer, double dur, int beeps)
+{
+  // Ensure Green and blue led are low
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(BLUE_LED, LOW);
+
+  // Play alarm beeps
+  for(int i = 0; i < beeps; i++)
+  {
+    digitalWrite(RED_LED, HIGH);
+    beep(freq,buzzer,dur);
+    digitalWrite(RED_LED, LOW);
+    delay(dur/2);
+  }
+}
+
 
 // Apply a low-pass filter to the given x,y, and z values
 void lowPassFilter(double* forces, double alpha)
