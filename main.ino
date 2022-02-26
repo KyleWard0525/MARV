dba9b3bbbb6176b7fbd0aab5b188a1475ad2598f
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-02-26 15:52:54 -0500

    Added driveSquare()

--- main.ino
+++ main.ino
@@ -20,6 +20,38 @@ uint16_t startPin = 74;       //  Button 2
 
 Marv* robot;
 
+uint32_t maxItrs = 1;
+uint8_t itrs = 0;
+
+// Drive square test
+void driveSquare(float sideLen)
+{
+  Serial.println("\nInitial heading: " + String(robot->imu->heading));
+  alarm(500, buzzerPin, 500, 3);
+  delay(1000);
+  robot->motors->forward(sideLen);
+  delay(1000);
+  robot->motors->turn(-90);
+  delay(1000);
+  robot->motors->forward(sideLen);
+  delay(1000);
+  robot->motors->turn(-90);
+  delay(1000);
+  robot->motors->forward(sideLen);
+  delay(1000);
+  robot->motors->turn(-90);
+  delay(1000);
+  robot->motors->forward(sideLen);
+  delay(1000);
+  robot->motors->turn(-90);
+  delay(1000);
+}
+
+// For IMU tests
+void imuTest()
+{
+  ;
+}
 
 // For testing new functionality
 void test()
@@ -31,18 +63,18 @@ void test()
   Serial.print("\tAy = " + String(arr[1]) + "g");
   Serial.print("\tAz = " + String(arr[2]) + "g\n");
 
-  double pr_arr[2];
-  robot->imu->getPitchRoll(pr_arr);
+  double pyr[3];
+  robot->imu->getPitchYawRoll(pyr);
 
-  Serial.print("\n\nPitch = " + String(pr_arr[0]) + " deg ");
-  Serial.print("\tRoll = " + String(pr_arr[1]) + " deg");;
+  Serial.print("\nPitch = " + String(pyr[0]) + " deg ");
+  Serial.print("\tYaw = " + String(pyr[1]) + " deg? ");
+  Serial.print("\tRoll = " + String(pyr[2]) + " deg\n");
 
-  double distance = robot->sonicSensor->measure();
-  Serial.println("\nDistance measured: " + String(distance));
+  Serial.println("\n\nSampling rate: " + String(robot->imu->getSamplingRate()) + "Hz");
 
-  free(arr);
-  free(pr_arr);
-  //robot->motors.forward(7);
+  delay(1000);
+  
+  //robot->displayPitchRoll();
 }
 
 void setup() {  
@@ -57,8 +89,7 @@ void setup() {
   }
   delay(1000);
   
-  Serial.println("Serial ready!");
-
+  Serial.print("Serial ready!\n");
 
   pinMode(startPin, INPUT_PULLUP);
 
@@ -84,15 +115,37 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
+
   robot = new Marv(buzzerPin, morseLed, trigPin, echoPin);
   
   robot->morse->i(); 
-  
+
+  delay(2000);
   Serial.println("\n\nEnd setup()\n");
 }
 
 void loop() {
+//  if(itrs >= maxItrs)
+//  {
+//    Serial.println("\nEnd of program\n");
+//    exit(0);
+//  }
+
+  if(digitalRead(startPin) == 0)
+  {
+    driveSquare(30.5*3);
+  }
 
   robot->checkBumpers();
+
+  // Print accels and gyro readings
+  //Serial.println("Ax = " + String(imuVals[0]) + "\tAy = " + String(imuVals[1]) + "\tAz = " + String(imuVals[2])
+                  //+ "\nGx = " + String(imuVals[3]) + "\tGy = " + String(imuVals[4]) + "\tGz = " + String(imuVals[5]) + "\n");
   
+  // Print IMU derivatives
+  //Serial.println("\nHeading = " + String(robot->imu->heading) + "deg\tVelocity = " + String(robot->imu->velocity) + " cm/s\tDistance traveled: " + String(robot->imu->distance) + " cm");
+  
+  delay(200);
+
+  itrs++;  
 }
