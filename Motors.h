2d80a7e669c8385452dd6571e695f9710d202167
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-02-26 15:51:16 -0500

    Update forward() and turn() to be more accurate

--- Motors.h
+++ Motors.h
@@ -7,7 +7,7 @@
 * 
 * kward
 */
-#include <Math.h>
+
 #include "RSLK_Pins.h"
 #include "Encoder.h"
 #include "Romi_Motor_Power.h"
@@ -16,20 +16,25 @@
 class Motors {
 
   private:
-    const uint16_t pulsesPerMotorRev = 3;             //  Pulses per revolution of the motor shaft
-    const uint16_t pulsesPerWheelRev = 1440;          //  Pulses per revolution of the wheels
-    const uint8_t wheelDiameter = 7;                  //  Wheel diameter in cm
-    const uint16_t gearRatio = 120;                   //  120:1 because ratio = (360 / pulses per motor revolution(3)) 
-    uint16_t defaultSpeed = 50;                       //  Default speed (for now)
-    double cmPerPulse;                                //  Centimeters traveled per pulse
+    const float pulsesPerMotorRev = 3;             //  Pulses per revolution of the motor shaft
+    const float pulsesPerWheelRev = 1440;          //  Pulses per revolution of the wheels
+    const float wheelDiameter = 6.999;             //  Wheel diameter in cm
+    const float wheelBase = 14;                //  Wheel base in cm
+    const float gearRatio = 120;                   //  120:1 because ratio = (360 / pulses per motor revolution(3)) 
+    const int defaultDriveSpeed = 35;               //  Default driving speed
+    const int defaultTurnSpeed = 50;                //  Default turning speed
+    double cmPerPulse;                             //  Centimeters traveled per pulse
     
 
   public:
     Romi_Motor_Power leftMotor;                       //  For controlling left wheel
     Romi_Motor_Power rightMotor;                      //  For controlling right wheel
     Bumpers bumpSensors;                              //  Bump sensor interface
-
-    Motors() 
+    IMU* imu;                                         //  Onboard IMU
+    double heading;                                   //  Current heading angle
+    uint16_t buzzerPin = 2;       //  GPIO pin for the buzzer
+      
+    Motors(IMU* mpu) 
     {
         // Compute cm traveled per pulse
         cmPerPulse = (wheelDiameter * M_PI) / (gearRatio * pulsesPerMotorRev);
@@ -42,13 +47,24 @@ class Motors {
         // Initialize motors
         leftMotor.begin(MOTOR_L_SLP_PIN,MOTOR_L_DIR_PIN,MOTOR_L_PWM_PIN);  // Params: sleep(enable) pin, direction pin, pwm pin
         rightMotor.begin(MOTOR_R_SLP_PIN,MOTOR_R_DIR_PIN,MOTOR_R_PWM_PIN); // Params: sleep(enable) pin, direction pin, pwm pin
+
+        // Initialize heading to 0
+        heading = 0;
+
+        // Set imu
+        imu = mpu;
     }
 
     
     void forward(double dist_cm)
     {      
+      unsigned long timestep = 0;
+      unsigned long prevTimeStep = 0;
+      double imuVals[6];
+      double startHeading = heading;
+      
       // Compute number of pulses needed
-      int nPulses = floor(dist_cm / cmPerPulse);
+      int nPulses = floor(dist_cm / cmPerPulse) + defaultDriveSpeed;
 
       // Reset motor encoder counts
       resetLeftEncoderCnt();
@@ -62,14 +78,48 @@ class Motors {
       rightMotor.enableMotor();
 
       // Set motor speed (start moving)
-      leftMotor.setSpeed(defaultSpeed);
-      rightMotor.setSpeed(defaultSpeed);
+      leftMotor.setSpeed(defaultDriveSpeed);
+      rightMotor.setSpeed(defaultDriveSpeed);
 
+      Serial.println("Pulses needed to travel " + String(dist_cm) + " cm = " + String(nPulses));
+
+      double prevGz = 0;
 
      // Wait for motor encoder to read nPulses (distance traveled = dist_cm)
      while(getEncoderLeftCnt() < nPulses && getEncoderRightCnt() < nPulses)
      {
-        // Do nothing
+        // Check for bumper collision
+        checkBumpers();
+          
+        imu->poll(imuVals);
+          
+        prevTimeStep = timestep;
+        timestep = (micros() - prevTimeStep) / 1000000.0;
+          
+        // Update heading
+        heading += (imuVals[5] - prevGz)*timestep;
+
+        // Check heading
+        if(heading > startHeading + 1 || heading < startHeading -  1)
+        {
+          // Check direction of error
+          if(heading < startHeading - 1)
+          {
+            // Off to the left, slow down right motor
+            leftMotor.setSpeed(int(defaultDriveSpeed / 2));
+          }
+          else if(heading > startHeading + 1)
+          {
+            // Off to the left, slow down right motor
+            rightMotor.setSpeed(int(defaultDriveSpeed / 2));
+          }
+          else {
+            continue;
+          }
+        }
+        
+        prevGz = imuVals[5];
+          
         delay(1);
      }
      
@@ -78,8 +128,8 @@ class Motors {
       leftMotor.disableMotor();
       
       
-      Serial.println("Pulses needed to travel " + String(dist_cm) + "cm = " + String(nPulses));
-      Serial.println("Actual pulses measured: L = " + String(getEncoderLeftCnt()) + " R = " + String(getEncoderRightCnt()));
+      //Serial.println("Pulses needed to travel " + String(dist_cm) + "cm = " + String(nPulses));
+      //Serial.println("Actual pulses measured: L = " + String(getEncoderLeftCnt()) + " R = " + String(getEncoderRightCnt()));
     }
 
     void reverse(double dist_cm)
@@ -99,14 +149,13 @@ class Motors {
       rightMotor.enableMotor();
 
       // Set motor speed (start moving)
-      leftMotor.setSpeed(defaultSpeed);
-      rightMotor.setSpeed(defaultSpeed);
+      leftMotor.setSpeed(defaultDriveSpeed);
+      rightMotor.setSpeed(defaultDriveSpeed);
 
 
      // Wait for motor encoder to read nPulses (distance traveled = dist_cm)
      while(getEncoderLeftCnt() < nPulses && getEncoderRightCnt() < nPulses)
      {
-        // Do nothing
         delay(1);
      }
      
@@ -119,8 +168,12 @@ class Motors {
     }
 
     // Turn a given number of degrees (negative=left turn, positive=right turn)
-    void turn(int16_t nDeg)
+    void turn(float nDeg)
     {
+      unsigned long timestep = 0;
+      unsigned long prevTimeStep = 0;
+      double imuVals[6];
+      
       // Error check
       if(nDeg < -359)
       {
@@ -134,9 +187,10 @@ class Motors {
       /* 
       *  Compute number of pulses needed
       *  
-      *  nPulses = (pulses per rev * abs(nDeg)) / 360
+      *  nPulses = (n + wheelbase)*pi / wheelDiameter*pi
       */
-      int16_t nPulses = (pulsesPerWheelRev * abs(nDeg)) / 360.0 / 2;
+     int nPulses = (((abs(nDeg) + wheelBase)*M_PI) / wheelDiameter*M_PI);
+
 
       // Reset motor encoder counts
       resetLeftEncoderCnt();
@@ -156,13 +210,33 @@ class Motors {
         rightMotor.enableMotor();
 
         // Set motor speed (start moving)
-        leftMotor.setSpeed(defaultSpeed);
-        rightMotor.setSpeed(defaultSpeed);
+        leftMotor.setSpeed(defaultTurnSpeed);
+        rightMotor.setSpeed(defaultTurnSpeed);
+        
+        double prevGz = 0;
 
         // Wait for motor encoder to read nPulses
-        while(getEncoderLeftCnt() < nPulses/2 || getEncoderRightCnt() < nPulses)
+        while(getEncoderLeftCnt() < nPulses || getEncoderRightCnt() < nPulses)
         {
-          // Do nothing
+          // Check for bumper collision
+.          checkBumpers();
+          
+          imu->poll(imuVals);
+          
+          prevTimeStep = timestep;
+          timestep = (micros() - prevTimeStep) / 1000000.0;
+          
+          // Update heading
+          heading += (imuVals[5] - prevGz)*timestep;
+          
+          if(getEncoderLeftCnt() % (nPulses/2))
+          {
+            leftMotor.setSpeed(int(defaultTurnSpeed/2));
+            rightMotor.setSpeed(int(defaultTurnSpeed/2));
+          }
+
+          prevGz = imuVals[5];
+          
           delay(1);
         }
 
@@ -175,6 +249,7 @@ class Motors {
       }
       else if(nDeg > 0)
       {
+          
         // Set motor directions to turn right
         leftMotor.directionForward();
         rightMotor.directionBackward();
@@ -184,18 +259,34 @@ class Motors {
         rightMotor.enableMotor();
 
         // Set motor speed (start moving)
-        leftMotor.setSpeed(defaultSpeed);
-        rightMotor.setSpeed(defaultSpeed);
+        leftMotor.setSpeed(defaultTurnSpeed);
+        rightMotor.setSpeed(defaultTurnSpeed);
+
+        double prevGz = 0;
 
         // Wait for motor encoder to read nPulses
         while(getEncoderLeftCnt() < nPulses || getEncoderRightCnt() < nPulses)
         {
+          // Check for bumper collision
+          checkBumpers();
+          
+          imu->poll(imuVals);
+          
+          prevTimeStep = timestep;
+          timestep = (micros() - prevTimeStep) / 1000000.0;
+          
+          // Update heading
+          heading += (imuVals[5] - prevGz)*timestep;
+          
           if(getEncoderLeftCnt() % (nPulses/2))
           {
-            leftMotor.setSpeed(defaultSpeed/2);
-            rightMotor.setSpeed(defaultSpeed/2);
+            leftMotor.setSpeed(defaultTurnSpeed/2);
+            rightMotor.setSpeed(defaultTurnSpeed/2);
           }
-          delayMicroseconds(200);
+
+          prevGz = imuVals[5];
+          
+          delay(1);
         }
 
         // Stop motors
@@ -206,18 +297,30 @@ class Motors {
         Serial.println("Actual pulses measured: L = " + String(getEncoderLeftCnt()) + " R = " + String(getEncoderRightCnt()));
       }
     }
+
+    // Check bumpers for collision and handle response
+    void checkBumpers()
+    {
+      if(bumpSensors.checkForCollision())
+      {
+        unsigned long start = millis();
+        
+        while(bumpSensors.checkForCollision())
+        {
+          //  Play alert
+          bumpSensors.alert(buzzerPin);
+
+          // Check if anyone has helped marv in the alloted time
+          if(millis() - start >= 250)
+          {
+            reverse(5); // Reverse ~5cm
+          }
+        }
+      } else {
+        bumpSensors.setStatusLed(1);
+      }
+    }
 };
-
-
-
-
-
-
-
-
-
-
-
 
 
 
