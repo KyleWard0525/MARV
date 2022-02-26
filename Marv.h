687a39368e182c292f42f2e1e0d2e8002eeb2010
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-02-26 15:52:39 -0500

    Minor changes

--- Marv.h
+++ Marv.h
@@ -6,6 +6,8 @@
  * 
  * kward
  */
+#include <stdio.h>
+#include <string.h>
 #include <Romi_Motor_Power.h>
 #include <Wire.h>
 #include <Math.h>
@@ -22,13 +24,15 @@
 #include "LCD.h"
 #include "Motors.h"
 
+using namespace std;
+
 // Main class for controlling the robot
 class Marv {
 
   private:
     uint16_t buzzerPin;                   //  GPIO pin for buzzer
     uint16_t morseLedPin;                 //  GPIO pin for onboard LED used for communicating in morse code
-
+    const float wheelBase = 14;           //  Wheel base in cm
     Bumpers bumpSensors;                  //  Interface for the bump sensors
     
     
@@ -42,10 +46,10 @@ class Marv {
       Bored,            //  Idle for long period of time
       Curious           //  'Go' mode. When marv wants to travel around and explore
     } feeling;
-
+    
   
     I2C serialBus;                        //  Reading and writing data of I2C channels
-    Motors motors;                        //  API for precise motor controls
+    Motors* motors;                       //  API for precise motor controls
     IMU* imu;                             //  Measuring acceleration and gyro forces
     Morse* morse;                         //  Communcation with the outside world through Morse code
     UltrasonicSensor* sonicSensor;        //  For measuring distance using ultrasound
@@ -58,25 +62,27 @@ class Marv {
       buzzerPin = buzzPin;
       morseLedPin = morsePin;
       
-
       // Set MARV's initial internal states
       feeling = Idle;
 
       // Initialize LCD screen
       LiquidCrystal_I2C lcdI2C_module(0x27,16,2);
-      lcd = new LCD(&lcdI2C_module);
-      
-      lcd->showMessage("Booting...", 2500);
+      this->lcd = new LCD(&lcdI2C_module);
+      Serial.println("LCD initialized.");
       
       // Initialize morse object
       morse = new Morse(buzzerPin, morseLedPin);
-
+      Serial.println("Morse initialized.");
+      
       // Initialize IMU
       imu = new IMU(&serialBus);
+      Serial.println("IMU initialized.");
+
+      //Initialize motors
+      motors = new Motors(imu);
       
       // Initialize ultrasonic sensor
       sonicSensor = new UltrasonicSensor(ePin, tPin);
-
       Serial.println("Ultrasonic sensor initialized.");
     }
 
@@ -97,7 +103,7 @@ class Marv {
           // Check if anyone has helped marv in the alloted time
           if(millis() - start >= 250)
           {
-            motors.reverse(5); // Reverse ~5cm
+            motors->reverse(5); // Reverse ~5cm
           }
         }
       } else {
@@ -110,6 +116,20 @@ class Marv {
     {
       // Check distance to nearest object
       
+    }
+
+    void displayPitchRoll()
+    {
+      delay(1000);
+      // Get pitch and roll
+      double pyr[3];
+      imu->getPitchYawRoll(pyr);
+
+      // Write pitch onto screen
+      const char* msg = String("Pitch: " + String(pyr[0])).c_str();
+      this->lcd->on();
+      delay(1000);
+      this->lcd->off();
     }
 };
 
