b359b4a5f371372c88ce070fc77af8f1a56c45de
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-03-19 13:32:23 -0400

    Started moving sensor functions in Motors.h

--- Marv.h
+++ Marv.h
@@ -8,13 +8,13 @@
  */
 #include <stdio.h>
 #include <string.h>
-#include <Romi_Motor_Power.h>
 #include <Wire.h>
 #include <Math.h>
+#include <Romi_Motor_Power.h>
+#include <LiquidCrystal_I2C.h>
 #include "Energia.h"
 #include "GPIO.h"
 #include "RSLK_Pins.h"
-#include "LiquidCrystal_I2C.h"
 #include "Utils.h"
 #include "Bumpers.h"
 #include "I2C.h"
@@ -32,23 +32,56 @@ class Marv {
   private:
     uint16_t buzzerPin;                   //  GPIO pin for buzzer
     uint16_t morseLedPin;                 //  GPIO pin for onboard LED used for communicating in morse code
+    uint16_t pirPin;                      //  Pin for PIR Sensor
     uint8_t forwardBufferDist;            //  Forward buffer distance in cm
     const float wheelBase = 14;           //  Wheel base in cm
     Bumpers bumpSensors;                  //  Interface for the bump sensors
-    
+
+
+    //  Struct for controlling MARV's "emotions" and corresponding behavior
+    struct Emotions {
+      // Enumerator for MARV's emotional state
+      enum EMOTIONAL_STATE {
+        Idle,             //  Main state, occurs when marv is not doing anything
+        Afraid,           //  Someone too close, marv stuck(long period), other
+        Angry,            //  Someone way too close, marv stuck(short period), continuously bumping, other
+        Bored,            //  Idle for long period of time
+        Curious           //  'Go' mode. When marv wants to travel around and explore
+      } feeling;
+
+      // Assign generic emotion type display LEDs 
+      uint16_t goodLed = GREEN_LED;     //  For good emotions
+      uint16_t neutralLed = BLUE_LED;   //  For neutral emotions
+      uint16_t badLed = RED_LED;        //  For bad emotions
+      uint16_t otherLed = YELLOW_LED;   //  For various other emotions
+
+      // Display emotion using external peripherals
+      void displayEmotion()
+      {
+        // Check current emotion
+        switch(feeling) {
+
+          // Idle, MARV is just vibing
+          case Idle:
+            // Enable 'Good' emotion LED
+            digitalWrite(goodLed, HIGH);
+            break;
+        }
+      }
+
+      // Play nervous beeps (intensity between 1-10)
+      void nervous(int intensity)
+      {
+        // Clamp intensity (error check)
+        intensity = clamp(intensity, 1, 10);
+
+        // Check intensity
+      }
+      
+    } emotions;
     
   public:
 
-    // Enumerator for MARV's emotional state
-    enum EMOTIONAL_STATE {
-      Idle,             //  Main state, occurs when marv is not doing anything
-      Afraid,           //  Someone too close, marv stuck(long period), other
-      Angry,            //  Someone way too close, marv stuck(short period), continuously bumping, other
-      Bored,            //  Idle for long period of time
-      Curious           //  'Go' mode. When marv wants to travel around and explore
-    } feeling;
-    
-  
     I2C serialBus;                        //  Reading and writing data of I2C channels
     Motors* motors;                       //  API for precise motor controls
     IMU* imu;                             //  Measuring acceleration and gyro forces
@@ -57,15 +90,16 @@ class Marv {
     LCD* lcd;                             //  Interface for LCD1602 screen
     
     //  Main constructor
-    Marv(uint16_t buzzPin, uint16_t morsePin, uint16_t tPin, uint16_t ePin)
+    Marv(uint16_t buzzPin, uint16_t morsePin, uint16_t tPin, uint16_t ePin, uint16_t irPin, LiquidCrystal_I2C* lcdI2C)
     { 
       // Set peripheral pins
       buzzerPin = buzzPin;
       morseLedPin = morsePin;
+      pirPin = irPin;
 
       // Initialize LCD screen
-      LiquidCrystal_I2C lcdI2C_module(0x27,16,2);
-      lcd = new LCD(&lcdI2C_module);
+      lcd = new LCD(lcdI2C);
+      lcd->showMessage("Booting...", -1, lcd->screenPadding, 0);   //  Show boot message
       Serial.println("LCD initialized.");
       
       // Initialize morse object
@@ -74,19 +108,16 @@ class Marv {
       
       // Initialize IMU
       imu = new IMU(&serialBus);
-      Serial.println("IMU initialized.");
-
-      //Initialize motors
-      motors = new Motors(imu);
       
       // Initialize ultrasonic sensor
       sonicSensor = new UltrasonicSensor(ePin, tPin);
       Serial.println("Ultrasonic sensor initialized.");
 
-      delay(500);
+      //Initialize motors
+      motors = new Motors(imu, sonicSensor);
 
       // Set MARV's initial internal states and variables
-      feeling = Idle;
+      emotions.feeling = emotions.Idle;
       forwardBufferDist = sonicSensor->forwardOffsetDist + 5; // 5cm buffer in front of robot
 
       // Signal that MARV is ready to go
@@ -126,33 +157,51 @@ class Marv {
       lcd->showMessage("Ready!", 1500, 5, 0);
     }
 
-    // Check distance to objects in front of marv and attempt to handle various situations
-    void monitorForwardSensor()
+    // Check objects in front of marv and attempt to handle various situations
+    void monitorForwardSensors()
     {
+      // Check PIR Sensor
+//      if(digitalRead(pirPin) == 1)
+//      {
+//        Serial.println("PIR Triggered!");
+//        alarm(300, buzzerPin, 500, 1, RED_LED);
+//      }
+      
       // Measure distance to nearest object
       long dist = sonicSensor->measure();
+      lcd->showMessage("Distance: " + String(dist) + "cm", -1, 0, 0); lcd->showMessage("Distance: " + String(dist) + "cm", -1, 0, 0); 
 
       // Check if object is within forward buffer zone
       if(dist <= forwardBufferDist)
       {
+        // Check if an ultrasonic measurement event has started
+        if(sonicSensor->avoids == 0)
+        {
+          // Start measurement event
+          sonicSensor->eventStart = millis();
+        }
+        
         // Reverse away from the object
         motors->reverse(5);
+
+        // Increment collisions avoided
+        sonicSensor->avoids++;
+
+        // Check number of avoidances (i.e. is an object still following MARV)
+        if(sonicSensor->avoids >= sonicSensor->warningLimit)
+        {
+          // Play warning
+        }
       }
     }
 
-    void displayPitchRoll()
-    {
-      delay(1000);
-      // Get pitch and roll
-      double pyr[3];
-      imu->getPitchYawRoll(pyr);
 
-      // Write pitch onto screen
-      const char* msg = String("Pitch: " + String(pyr[0])).c_str();
-      this->lcd->on();
-      delay(1000);
-      this->lcd->off();
-    }
+   void say(String msg)
+   {
+    lcd->showMessage("Hi", 1000, 1, 0);
+   }
+
+    
 };
 
 
