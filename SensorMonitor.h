e56102534be6bd1ba213474affff5e5e3f4bdf3a
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-03-19 13:44:56 -0400

    For monitoring sensors and handling trigger events

---
+++ SensorMonitor.h
@@ -0,0 +1,35 @@
+#ifndef SENSOR_MONITOR_H
+#define SENSOR_MONITOR_H
+
+/**
+ * This class is the main interface for monitoring all of MARV's sensors
+ * 
+ * kward
+ */
+#include "Utils.h"
+#include "Bumpers.h"
+#include "I2C.h"
+#include "IMU.h"
+#include "Morse.h"
+#include "Ultrasonic.h"
+#include "LCD.h"
+#include "Motors.h"
+
+class SensorMonitor 
+{
+  private:
+
+  public: 
+};
+
+
+
+
+
+
+
+
+
+
+
+#endif  //  End SENSOR_MONITOR_H
