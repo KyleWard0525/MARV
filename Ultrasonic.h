00aeed779c8a2ddeddeaef4f1ed7b62358f96d1c
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-04-09 13:23:56 -0400

    Updated to take 6 measurements and return their median. For better accuracy

--- Ultrasonic.h
+++ Ultrasonic.h
@@ -31,32 +31,41 @@ class UltrasonicSensor {
   //  Poll distance to nearest object from the sensor (cm)
   long measure()
   {
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    /*
-     * Set the trigger pin to high for 10 microseconds as the trigger pin
-     * is only activated when a 10 microsecond pulse is applied
-     */
-     digitalWrite(trigPin, HIGH);
-     delayMicroseconds(10);
-     digitalWrite(trigPin, LOW);
+    long distances[6];
 
-     // Read the amoount of time taken in microseconds for echo pin to recieve a signal
-     long duration = pulseIn(echoPin, HIGH);
- 
-     /*
-      * In order to convert the time taken into cm, we must divide the result
-      * by 2 then again by 29.1. 
-      * 
-      * We divide by 2 as the duration contains the amount of time taken
-      * for the signal to travel from the target and back but we only want to measure the distance
-      * to the target.
-      * 
-      * Next, we divide by 29.1 because the speed of sound is 340m/s which is approximately
-      * 29.1 microseconds per centimeter.
-      * 
-      */
-      return (duration / 2) / 29.1;
+    for(int i = 0; i < 6; i++)
+    {
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(2);
+      /*
+       * Set the trigger pin to high for 10 microseconds as the trigger pin
+       * is only activated when a 10 microsecond pulse is applied
+       */
+       digitalWrite(trigPin, HIGH);
+       delayMicroseconds(10);
+       digitalWrite(trigPin, LOW);
+  
+       // Read the amoount of time taken in microseconds for echo pin to recieve a signal
+       long duration = pulseIn(echoPin, HIGH);
+   
+       /*
+        * In order to convert the time taken into cm, we must divide the result
+        * by 2 then again by 29.1. 
+        * 
+        * We divide by 2 as the duration contains the amount of time taken
+        * for the signal to travel from the target and back but we only want to measure the distance
+        * to the target.
+        * 
+        * Next, we divide by 29.1 because the speed of sound is 340m/s which is approximately
+        * 29.1 microseconds per centimeter.
+        * 
+        */
+        distances[i] = (duration / 2) / 29.1;
+    }
+    
+    // Sort distances and return median
+    sort(distances, sizeof(distances)/sizeof(distances[0]));
+    return distances[2];
   }
 };
 
