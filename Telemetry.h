223d67f107a43d13f3bbff585026e59d9c97178b
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-04-11 16:54:16 -0400

    Added IMU_Session.at()

--- Telemetry.h
+++ Telemetry.h
@@ -9,6 +9,7 @@
 #include "Utils.h"
 
 
+
 /**
  * High-level class for managing telemetry data
  */
@@ -32,10 +33,31 @@ class IMU_Session {
     unsigned long endTime;                    //  End time for the session
     unsigned long prevTimepoint;              //  Timepoint of last measurement
     uint32_t currIdx;                         //  Current measurement index
-    uint32_t capacity;                        //  Number of elements that can be stored in data
+    uint32_t szMax;                           //  Max number of elements that can be stored in data
+
+    // Initializer function
+    void init()
+    {
+     // Set session start time
+     startTime = millis();
+      
+     // Set default values for variables
+     currIdx = -1;
+     velocity = 0;
+     distance = 0;
+     samples = 0;
+     prevTimepoint = 1;
+
+     // Set default sample rate to 80Hz
+     sampleRateMs = hertzToMilliseconds(80);
+
+     // Set max list size
+     szMax = 500;
+    }
+    
 
   public:
-    uint32_t measurements;                    //  Number of measurements made/will be made
+    uint32_t szData;                          //  Initial size for data list
     uint32_t sampleRateMs;                    //  Delay in ms between samples
     uint32_t samples;                         //  Number of samples taken
     double velocity;                          //  Current velocity
@@ -43,25 +65,34 @@ class IMU_Session {
     imu_vals_t* data;                         //  IMU data for each measurement
     
     // Main constructor
-    IMU_Session(uint32_t n_measures)
-    {
-      // Set class variables
-      startTime = millis();
-      measurements = n_measures;
-      data = new imu_vals_t[measurements];
-      currIdx = -1;
-      velocity = 0;
-      distance = 0;
-      samples = 0;
-      prevTimepoint = 1;
-      capacity = measurements;
+    IMU_Session(uint32_t n_size)
+    { 
+      // Initialize
+      init();
+
+      // Check if n_size is in range
+      if(n_size > szMax)
+      {
+        // Set size of data to be max size
+        szData = szMax;
+        Serial.println("\n\nWARNING in IMU_Session(): given size of " + String(n_size) + " > max size = " + String(szMax) + "\nData size set to: " + String(szData)); 
+      }
+      else {
+        // Set initial size
+        szData = n_size;
+      }
+      
+      // Initialize internal data array
+      data = new imu_vals_t[szData];
     }
+
+    
 
     // Add IMU data to the end of the data list
     void push(imu_vals_t imu_data)
     {
       // Check if there's enough space in the list
-      if(currIdx + 1 < measurements)
+      if(currIdx + 1 < szData)
       {
         // Update current index and store data
         currIdx++;
@@ -82,9 +113,8 @@ class IMU_Session {
     // Resize internal data array
     void resize()
     {
-      uint32_t startSize = capacity;            //  Get size of data
+      uint32_t startSize = szData;            //  Get size of data
       uint32_t newSize = startSize * 2;         //  Compute new array size
-
       
 
       // Create new, larger array
@@ -98,19 +128,26 @@ class IMU_Session {
       }
 
       // Set internal data array to newly sized array
-      this->data = newData;
+      data = newData;
 
       // Update capacity
-      capacity = newSize;
-      Serial.println("\nIMU_Session resized to hold " + String(capacity) + " samples");
+      szData = newSize;
     }
 
-    // Return length of the list
-    uint32_t getCapacity()
+    // Return the imu_val struct at the corresponding index
+    imu_vals_t at(uint32_t idx)
     {
-      return capacity;
-    }
+      // Validate index
+      if(!range_check(idx, 0, szData-1))
+      {
+        Serial.println("\n\nERROR in IMU_Session.at(): index " + String(idx) + " out of bounds for list of size " + String(szData));
+        exit(0);
+      }
 
+      // Return imu_vals
+      return data[idx];
+    }
+    
     // Get start time
     unsigned long getStartTime()
     {
