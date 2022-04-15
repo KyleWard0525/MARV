0d41cee3825d63a3425f9cb37c6bb8869eae19d9
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2022-04-15 15:47:24 -0400

    Added sensor functions from Motors.h

--- SensorController.h
+++ SensorController.h
@@ -46,7 +46,67 @@ class SensorController {
       // Initialize servo
       servo.attach(sensor_pins.servoPin);
     }
-  
+
+    // Check bumpers for collision and handle response
+    // TODO: CHECK AND RETURN WHICH BUMPER COLLIDED
+    void checkBumpers()
+    {
+      if(bumpers.checkForCollision())
+      {
+        unsigned long start = millis();
+        
+        while(bumpers.checkForCollision())
+        {
+          //  Play alert
+          bumpers.alert(sensor_pins.buzzer);
+
+          // Check if anyone has helped marv in the alloted time
+          //reverse(5); // Reverse ~5cm
+        }
+      } else {
+        bumpers.setStatusLed(1);
+      }
+    }
+
+    // Monitor forward sensors
+    // Check objects in front of marv and attempt to handle various situations
+    // TODO: Determine a way to return necessary data
+    void monitorForwardSensors()
+    {
+      // Check PIR Sensor
+
+      // Check bumpers
+      checkBumpers();
+      long dist = frontSonicSensor->measure();
+      
+      // Check if object is within forward buffer zone
+      if(dist <= frontSonicSensor->offsetDist + frontSonicSensor->bufferDist)
+      {
+        // Reverse away from the object
+        //reverse(5);
+      }
+    }
+
+    // Perform an environmental sweep (assuming the sonic sensor is mounted on top of the servo
+  void servoSweep(uint16_t startPos, uint16_t endPos, uint16_t stepSize, long* outputs, uint32_t delayMs=100)
+  {
+    // Array index (since the loop is using values that may not be from 0-length of array)
+    uint16_t arrIdx = 0;
+    
+    // Loop through specified sweep range
+    for(uint16_t i = startPos; i <= endPos; i++)
+    {
+      // Move servo and store measurements in return array (outputs)
+      servo.write(i);
+      delay(delayMs/2);
+      outputs[arrIdx] = frontSonicSensor->measure();
+      delay(delayMs/2);
+      arrIdx++;
+    }
+
+    // Return servo to face forward
+    servo.write(90);
+  }
 };
 
 
